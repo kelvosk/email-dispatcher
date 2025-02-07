5c9ad28476afc4a3e4a86ce9d98e777f93bc6a48
Kelvin Gomes <kelvinrg3@gmail.com>
2025-02-07 01:54:33 +0000

    first commit

---
+++ src/test/java/com/email/dispatcher/EmailDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
